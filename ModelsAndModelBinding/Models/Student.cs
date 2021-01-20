6060ea53052e208012e096e6049ec96db39d312c
Kramer Jensen <Kramerjensend@gmail.com>
2021-01-19 17:14:24 -0800

    Updated Displays and DataTypes so I can display as I want

--- ModelsAndModelBinding/Models/Student.cs
+++ ModelsAndModelBinding/Models/Student.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -18,21 +19,27 @@ namespace ModelsAndModelBinding.Models
         /// <summary>
         /// The student's first and last name of student
         /// </summary>
+        [Display(Name="Full Name:")]
         public string FullName { get; set; }
 
         /// <summary>
         /// The day the student was born. Time is ignored
         /// </summary>
+        [Display(Name="Date of Birth:")]
+        [DataType(DataType.Date)]
         public DateTime DateOfBirth { get; set; }
 
         /// <summary>
         /// The student email (ends with @student.cptc.edu)
         /// </summary>
+        [Display(Name="Email Address:")]
+        [DataType(DataType.EmailAddress)]
         public string EmailAddress { get; set; }
 
         /// <summary>
         /// Primary phone number
         /// </summary>
+        [Display(Name="Phone Number:")]
         public string PhoneNumber { get; set; }
     }
 }
